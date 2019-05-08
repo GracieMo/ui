a8e74882ad2b88e3b2edef77b9af505d2a2278d1
GracieMo <love397378357@163.com>
2019-05-08 17:53:48 +0800

    haha

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
       <span style="padding-right:5px; font-size:15px; color:#22B8EB">上海机构1</span>
       <span style="padding-right:30px; font-size:15px; color:#22B8EB">·丨</span>
       <el-dropdown trigger="click">
-        <i class="iconfont icon-iconfontgerenzhongxin" style="margin-right: 15px; color:#22B8EB; "></i>
+        <i class="iconfont icon-iconfontgerenzhongxin" style="cursor:pointer; margin-right: 15px; color:#22B8EB; "></i>
         <el-dropdown-menu slot="dropdown" style="width:240px; margin-top:4px;border:none;padding-bottom:5px; ">
           <el-dropdown-item style="line-height:34px; margin-bottom:5px">
             <i class="iconfont icon-iconfontgerenzhongxin" style="padding-left: 10px; color:#22B8EB"></i>
@@ -51,8 +51,86 @@
       <!--主体界面-->
       <el-main>
         <div class='main'>
-          <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="未完成预约" name="first">未完成预约</el-tab-pane>
+          <!--标签页-->
+          <el-tabs v-model="activeName" @tab-click="handleClick"> 
+            <el-tab-pane label="未完成预约" name="first">
+              <!--未完成预约头部-->
+              <div style="text-align:left;">
+                <span style="color:#373737; font-size:25px;margin-left:25px;">未完成预约</span>
+                <span style="color:#7A7B7B; font-size:17px; padding: 18px 5px 20px 5px;">Appointment</span>
+                <i class="iconfont icon-jia" style="color:#22B8EB; font-size:25px; padding-left:5px; cursor:pointer;"></i>
+              </div>
+              <!--未完成预约表格-->
+              <div style="margin:80px 35px 0 25px; height:100%">
+                <el-table
+                    :data="UNFreservation"
+                    border
+                    >
+                    <el-table-column
+                      type="index"
+                      width="50">
+                    </el-table-column>
+                    <el-table-column
+                      prop="reserve_time"
+                      label="咨询时间"
+                      width="200">
+                    </el-table-column>
+                    <el-table-column
+                      prop="room"
+                      label="咨询室"
+                      width="100">
+                    </el-table-column>
+                    <el-table-column
+                      prop="counselor"
+                      label="咨询师"
+                      width="100">
+                    </el-table-column> 
+                    <el-table-column
+                      prop="entry_pereson"
+                      label="助理"
+                      width="100">
+                    </el-table-column>                                                                                   
+                    <el-table-column
+                      prop="visitor_name"
+                      label="姓名"
+                      width="150">
+                    </el-table-column>                    
+                    <el-table-column
+                      prop="visitor_sex"
+                      label="性别"
+                      width="100">
+                    </el-table-column>
+                    <el-table-column
+                      prop="visitor_phone"
+                      label="电话"
+                      width="180">
+                    </el-table-column>
+                    <el-table-column
+                      prop="birthday"
+                      label="出生日期"
+                      width="180">
+                    </el-table-column>
+                    <el-table-column
+                      prop="consult_times"
+                      label="咨询次数"
+                      width="100">                      
+                    </el-table-column>
+                    <el-table-column
+                      prop="entry_time"
+                      label="预约时间"
+                      width="180">                      
+                    </el-table-column>   
+                    <el-table-column label="操作" fixed="right">
+                        <el-button
+                          class="listbutton"                       
+                          @click="handleEdit(scope.$index, scope.row)">查看</el-button>
+                        <el-button
+                          size="mini"
+                          @click="handleDelete(scope.$index, scope.row)">编辑</el-button>                  
+                    </el-table-column>                                                                                    
+                  </el-table>                                
+              </div>
+            </el-tab-pane>
             <el-tab-pane label="已完成预约" name="second">已完成预约</el-tab-pane>
           </el-tabs>
         </div>
@@ -70,14 +148,35 @@ export default {
   export default {
     data() {
       return {
-        activeName: 'first'
-      };
+        /* 进入页面时默认进入未完成预约标签页 */
+        activeName: 'first',
+        /* 未完成预约列表数据 */
+        UNFreservation: [{
+          reserve_time: '2016-05-02  16:00~17:00',
+          room: '咨询室',
+          counselor: '陈七',
+          entry_pereson: '琪琪',
+          visitor_name: '张三',
+          visitor_sex: '男',
+          visitor_phone: '13466763626',
+          birthday: '1993-09-27',
+          consult_times: '2',
+          entry_time: '2016-05-02 16:30:27',
+        }]
+      }
     },
     methods: {
+      handleEdit(index, row) {
+
+      },
+      handleDelete(index, row) {
+
+      },
       handleClick(tab, event) {
+
       }
     }
-  };
+  }  
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -94,11 +193,11 @@ export default {
     background-color: #ffffff;
     text-align: center;
   }
-  /* 主体界面样式 */
+  /* 页面主体背景 */
   .el-main {
     background-color: #F7F7F7;
     text-align: center;
-    padding:15px 15px 15px 15px;
+    padding:0;
   }
 
   /* 侧边导航样式：展开后 */
@@ -129,10 +228,12 @@ export default {
     background-color: #FFFFFF;
     color:#9F9F9F;
   }
+  /* 页面主体 */ 
   .main{
     background-color: #fff;
     height:100%;
-    border: solid 1px #F0F0F0;    
+    border: solid 1px #F0F0F0;
+    margin:15px 15px 15px 15px;        
   }
 
   .el-container:nth-child(5) .el-aside,
@@ -143,25 +244,59 @@ export default {
   .el-container:nth-child(7) .el-aside {
     line-height: 320px;
   }
-  .el-tabs__item.is-active{
-    color:#22B8EB;
+  /* 标签页header */
+  .el-tabs__header{
+    margin:10px 35px 30px 0;
+
   }
+  /* 标签页文字：未选中时 */
   .el-tabs__item{
-    padding:0 25px;
+    padding:0 20px;
     height:40px;
+    left:25px;
     line-height: 40px;
     font-size:14px;
     font-weight:500;
     color:#555555;
     position:relative;   
   }
+  /* 标签页文字：hover */
+  .el-tabs__item:hover{
+    color:#22B8EB;
+  }
+  /* 标签页文字：选中时 */
+  .el-tabs__item.is-active{
+    color:#22B8EB;
+    left:25px;
+  }
+  /* 标签页线：激活时底线 */
+  .el-tabs__active-bar{
+    position: absolute;
+    bottom:0;
+    left:25px;
+    height:2px;
+    background-color:#22B8EB;
+  }
+  /* 标签页线：底线 */
   .el-tabs__nav-wrap::after{
     content:"";
     position:absolute;
     left:25px;
-    padding-right:80px;
     height:2px;
     background-color: #F0F0F0;
     z-index:1;
   }
+  .el-table{
+    position:relative; 
+    overflow:hidden;
+    box-sizing:border-box;
+    height:100%;
+  }  
+  .el-table{
+    position:relative; 
+    overflow:hidden;
+    box-sizing:border-box;
+    height:100%;    
+  }
+
 </style>
